e6c5188d5a53bb9d4362bd1effdff4f962b6ecd7
faozibouybaouenegadrouz <fbg171@gmail.com>
2022-05-07 11:36:10 +0200

    Strings update, and development of creation of event

--- app/src/main/java/com/example/openevents/CreateEventActivity.java
+++ app/src/main/java/com/example/openevents/CreateEventActivity.java
@@ -1,14 +1,44 @@
 package com.example.openevents;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class CreateEventActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-public class CreateEventActivity extends AppCompatActivity {
+    String[] categories = {"Cat1", "Cat2", "Cat3", "Cat4"};
+    Spinner spinnerCategories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+
+        configureSpinner();
+
+    }
+
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    private void configureSpinner() {
+        spinnerCategories = findViewById(R.id.sp_create_event_categories);
+        spinnerCategories.setOnItemSelectedListener(this);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, categories);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCategories.setAdapter(adapter);
     }
 }
