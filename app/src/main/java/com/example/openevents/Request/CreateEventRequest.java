38956d3c6a34b5d0d9f25d38bea2b374d38bc834
faozibouybaouenegadrouz <fbg171@gmail.com>
2022-05-09 19:27:19 +0200

    create even request creation

---
+++ app/src/main/java/com/example/openevents/Request/CreateEventRequest.java
@@ -0,0 +1,24 @@
+package com.example.openevents.Request;
+
+public class CreateEventRequest {
+
+    private String name;
+    private String image;
+    private String location;
+    private String description;
+    private String eventStart_date;
+    private String eventEnd_date;
+    private String n_participants;
+    private String type;
+
+    public CreateEventRequest(String name, String image, String location, String description, String eventStart_date, String eventEnd_date, String n_participants, String type) {
+        this.name = name;
+        this.image = image;
+        this.location = location;
+        this.description = description;
+        this.eventStart_date = eventStart_date;
+        this.eventEnd_date = eventEnd_date;
+        this.n_participants = n_participants;
+        this.type = type;
+    }
+}
