7353bd3fae93d85f7998d14481381827c35871f2
guillemmiro <guillem.miro@students.salle.url.edu>
2022-05-10 18:51:48 +0200

    Applied correct code format to EditProfileActivity

--- app/src/main/java/com/example/openevents/EditProfileActivity.java
+++ app/src/main/java/com/example/openevents/EditProfileActivity.java
@@ -3,30 +3,28 @@ package com.example.openevents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.example.openevents.Request.EditUserRequest;
+import com.example.openevents.Response.UserResponse;
+
 public class EditProfileActivity extends AppCompatActivity {
+
+    private EditText etName, etLastname, etEmail, etPassword1, etPassword2, etProfilePicture;
+    Button btnEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
-        EditText etName = findViewById(R.id.et_edit_name);
-        EditText etLastname = findViewById(R.id.et_last_name);
-        EditText etEmail = findViewById(R.id.et_email);
-        EditText etPassword1 = findViewById(R.id.et_password1);
-        EditText etPassword2 = findViewById(R.id.et_password2);
-        EditText etProfilePicture = findViewById(R.id.et_profile_picture);
+        setViews();
+        setUserData();
 
-        etName.setText(getIntent().getStringExtra("user_name"));
-        etLastname.setText(getIntent().getStringExtra("last_name"));
-        etEmail.setText(getIntent().getStringExtra("email"));
-        etProfilePicture.setText(getIntent().getStringExtra("profile_picture"));
-
-        Button btnEdit = findViewById(R.id.bt_edit_profile);
         btnEdit.setOnClickListener(v -> {
             String name = etName.getText().toString();
             String lastname = etLastname.getText().toString();
@@ -51,11 +49,29 @@ public class EditProfileActivity extends AppCompatActivity {
         });
     }
 
-    private void requestWithPassword(String name, String lastname, String email, String password1, String profilePicture) {
+    private void setViews() {
+        etName = findViewById(R.id.et_edit_name);
+        etLastname = findViewById(R.id.et_last_name);
+        etEmail = findViewById(R.id.et_email);
+        etPassword1 = findViewById(R.id.et_password1);
+        etPassword2 = findViewById(R.id.et_password2);
+        etProfilePicture = findViewById(R.id.et_profile_picture);
 
+        btnEdit = findViewById(R.id.bt_edit_profile);
+    }
+
+    private void setUserData() {
+        etName.setText(getIntent().getStringExtra("user_name"));
+        etLastname.setText(getIntent().getStringExtra("last_name"));
+        etEmail.setText(getIntent().getStringExtra("email"));
+        etProfilePicture.setText(getIntent().getStringExtra("profile_picture"));
+    }
+
+    private void requestWithPassword(String name, String lastname, String email, String password1, String profilePicture) {
+        EditUserRequest editProfileRequest = new EditUserRequest(name, lastname, email, password1, profilePicture);
     }
 
     private void requestWithoutPassword(String name, String lastname, String email, String profilePicture) {
-
+        EditUserRequest editProfileRequest = new EditUserRequest(name, lastname, email, profilePicture);
     }
 }
